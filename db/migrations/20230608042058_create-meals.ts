cb3db359096cee3c0ab58c94e5340df3a99f613c
Matheus Martins <matheusms280100@gmail.com>
2023-06-08 01:52:09 -0300

    create user and meals migrations

---
+++ db/migrations/20230608042058_create-meals.ts
@@ -0,0 +1,16 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.text('description').notNullable()
+    table.boolean('is_on_the_diet').notNullable()
+    table.timestamp('selected_date').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
